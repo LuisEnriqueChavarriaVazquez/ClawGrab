8fae1a5ecae9d7e9b59a03658da78621f45cf9fc
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2021-01-09 21:23:39 -0600

    actualziacion sign up

---
+++ conexionDB.php
@@ -0,0 +1,10 @@
+<?php 
+
+$dbServername="localhost";
+$dbUsername="root";
+$dbPassword="ramv1357";
+$dbName="ClawGrab";
+
+$conn=mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+?>
