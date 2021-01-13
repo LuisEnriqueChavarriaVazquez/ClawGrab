0d1f19b257142f53555c36f55d55e4d2b2cb492a
Luis Enrique <luisechvz@gmail.com>
2021-01-13 07:18:46 -0600

    Cambios nuevos para heroku

--- L_conectDatabase.php
+++ L_conectDatabase.php
@@ -1,9 +1,16 @@
 <?php 
    
-    $servername = "localhost";  
+    /*$servername = "localhost";  
     $username = "root";  
     $password = "12345"; 
-    $database = "clawgrab"; 
+    $database = "clawgrab"; */
+
+    $servername = "us-cdbr-east-03.cleardb.com";  
+    $username = "b3cf231598121b";  
+    $password = "bda8defc"; 
+    $database = "heroku_e99f66b5b833c37"; 
+
+    //mysql:b3cf231598121b:bda8defc@us-cdbr-east-03.cleardb.com/heroku_e99f66b5b833c37?reconnect=true
    
      // Create a connection  
      $conn = mysqli_connect($servername,  
